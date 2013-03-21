a6ef3c1d767a1a9970a19194859b485a358dfd5f
Aleksandar Gargenta <sasa@gargenta.com>
2013-03-21 10:39:01 -0700

    Added basic infrastructure

---
+++ src/com/marakana/addressbook/AddressBookException.java
@@ -0,0 +1,16 @@
+
+package com.marakana.addressbook;
+
+public class AddressBookException extends Exception {
+
+    private static final long serialVersionUID = -5404157887713123901L;
+
+    public AddressBookException(String message) {
+        super(message);
+    }
+
+    public AddressBookException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
---
+++ src/com/marakana/addressbook/FileBasedAddressBook.java
@@ -0,0 +1,31 @@
+
+package com.marakana.addressbook;
+
+//TODO: implement!
+public class FileBasedAddressBook implements AddressBook {
+
+    @Override
+    public Contact getByEmail(String email) throws AddressBookException {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+    @Override
+    public Contact[] getAll() throws AddressBookException {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+    @Override
+    public void store(Contact contact) throws AddressBookException {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+    @Override
+    public void deleteByEmail(String email) throws AddressBookException {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+
+    @Override
+    public void close() throws AddressBookException {
+        throw new UnsupportedOperationException("Implement me!");
+    }
+}
